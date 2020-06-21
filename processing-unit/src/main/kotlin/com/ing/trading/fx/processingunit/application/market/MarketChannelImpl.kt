7f0bae2c5d2d267ce8be792328e31902dbe4fbff
dinabogdan <bogdan.dina03@gmail.com>
2020-06-21 15:34:42 +0300

    add implementation for market channel in processing unit

--- processing-unit/src/main/kotlin/com/ing/trading/fx/processingunit/application/market/MarketChannelImpl.kt
+++ processing-unit/src/main/kotlin/com/ing/trading/fx/processingunit/application/market/MarketChannelImpl.kt
@@ -1,14 +1,33 @@
 package com.ing.trading.fx.processingunit.application.market
 
+import com.ing.fx.trading.marketclient.api.model.PublishFailure
+import com.ing.fx.trading.marketclient.api.model.PublishResult
+import com.ing.fx.trading.marketclient.api.model.PublishSuccess
 import com.ing.fx.trading.marketclient.api.model.Quote
 import com.ing.fx.trading.marketclient.api.service.MarketChannel
 import com.ing.trading.fx.processingunit.domain.QuotesRepository
+import java.lang.RuntimeException
 
 class MarketChannelImpl(
         private val quotesRepository: QuotesRepository
 ) : MarketChannel {
-
-    override fun publish(quotes: List<Quote>) {
-        quotesRepository.addAll(quotes)
+    override fun publish(quote: Quote): PublishResult<Quote> {
+        return try {
+            quotesRepository.add(quote)
+            PublishSuccess(quote)
+        } catch (ex: Exception) {
+            PublishFailure(RuntimeException(ex))
+        }
     }
+
+    override fun publish(quotes: List<Quote>): PublishResult<List<Quote>> {
+        return try {
+            quotesRepository.addAll(quotes)
+            PublishSuccess(quotes)
+        } catch (ex: Exception) {
+            PublishFailure(RuntimeException(ex))
+        }
+    }
+
+
 }
